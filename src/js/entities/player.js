494b20990e38609634e3a2374d53d11c6b30a14b
Pedro Henrique <pedrohenriquerls@gmail.com>
2014-12-06 12:21:30 -0200

    simple animation

--- src/js/entities/player.js
+++ src/js/entities/player.js
@@ -7,8 +7,9 @@ var Player = function (game, x, y) {
   this.cursors = game.input.keyboard.createCursorKeys();
 
   game.add.existing(this);
-
   game.camera.follow(this);
+
+  this.loadAnimations()
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -24,20 +25,31 @@ Player.prototype.update = function() {
 
   if(this.cursors.up.isDown) {
     this.body.velocity.y -= 150;
+    this.animations.play('walkTop', 30);
   }
   else if(this.cursors.down.isDown) {
     this.body.velocity.y += 150;
+    this.animations.play('walkBottom', 30);
   }
   if(this.cursors.left.isDown) {
     this.body.velocity.x -= 150;
+    this.animations.play('walkLeft', 30);
   }
   else if(this.cursors.right.isDown) {
     this.body.velocity.x += 150;
+    this.animations.play('walkRight', 30);
   }
 }
 
 Player.prototype.addCollider = function(game, layer){
   game.physics.arcade.collide(this, layer);
+}
+
+Player.prototype.loadAnimations = function(){
+  this.animations.add('walkBottom', [0,1], 10);
+  this.animations.add('walkTop', [2,3], 10);
+  this.animations.add('walkRight', [4,5], 10);
+  this.animations.add('walkLeft', [6,7], 10);
 }
 
 

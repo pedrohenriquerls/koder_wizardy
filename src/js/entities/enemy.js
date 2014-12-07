3200c9506e540e54768f313f6f6ed389ba3e1035
Pedro Henrique <pedrohenriquerls@gmail.com>
2014-12-07 03:57:31 -0200

    poor ai for enemys

--- src/js/entities/enemy.js
+++ src/js/entities/enemy.js
@@ -1,11 +1,28 @@
 var SkullProfiles = require('../profiles/skulls');
 
 var Enemy = function (sprite) {
-  sprite.body.moves = false;
-  sprite.body.collideWorldBounds = true
+  //sprite.body.moves = false;
   sprite.physicsBodyType = Phaser.Physics.ARCADE;
+  sprite.body.collideWorldBounds = true
 
-  this.alive = true
+  sprite.target = window.player;
+
+  // Set the pivot point for this sprite to the center
+  sprite.anchor.setTo(0.5, 0.5);
+
+  // Enable physics on this object
+  sprite.game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+  // Define constants that affect motion
+  sprite.MAX_SPEED = 50; // pixels/second
+  sprite.MIN_DISTANCE = 0; // pixels
+
+  sprite.animations.add('walkBottom', [0,1], 10);
+  sprite.animations.add('walkTop', [2,3], 10);
+  sprite.animations.add('walkRight', [4,5], 10);
+  sprite.animations.add('walkLeft', [6,7], 10);
+
+  sprite.alive = true
 
   this.profileGenerator()
   var self = this
@@ -18,7 +35,27 @@ var Enemy = function (sprite) {
   		window.player.clearFightScene()
 
   		this.destroy()
-  	}
+  	}else{
+      var distance = this.game.math.distance(this.x, this.y, this.target.x, this.target.y);
+      if(distance <= 60){
+        // If the distance > MIN_DISTANCE then move
+        if (distance > this.MIN_DISTANCE) {
+            // Calculate the angle to the target
+            var rotation = this.game.math.angleBetween(this.x, this.y, this.target.x, this.target.y);
+            if(rotation > 1)
+              this.animations.play('walkBottom', 6);
+            else
+              this.animations.play('walkTop', 6);
+
+            // Calculate velocity vector based on rotation and this.MAX_SPEED
+            this.body.velocity.x = Math.cos(rotation) * this.MAX_SPEED;
+            this.body.velocity.y = Math.sin(rotation) * this.MAX_SPEED;
+        } else {
+            this.body.velocity.setTo(0, 0);
+        }
+      }else
+         this.body.velocity.setTo(0, 0);
+    }
   }
 }
 

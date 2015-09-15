ec12aec5a12682bddbf836c07ddea81acaf24d76
Pedro Henrique <pedro.lima@s2it.com.br>
2015-09-15 06:45:14 -0300

    correção para ghpages

--- src/js/profiles/tutorials.js
+++
@@ -1,6 +0,0 @@
-var Tutorials = [
-	'Yoda about JS: The Javascript a powerful ally it is. Brendan creates it,\n makes it grow. Its energy surrounds us and binds us. Objects beings are\nwe, not this crude matter. You must debug the Objects around you; here,\nbetween you, me, the nodejs, the jquery, everywhere, yes.\n',
-	'Like Yoda told ya, this first fight is only about objects, try debug\nyour enemies(debug(enemy)).\nFind a away to defeat him, using the power of Javascript.\n Tip: enemy.name = "blastoise"\n'
-]
-
-module.exports = Tutorials
